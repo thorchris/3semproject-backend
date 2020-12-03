b53799ae97f6b4608c3195fd938db208df786776
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-12-03 13:42:09 +0100

    Search for character name (SPECIFIC NAME)
    Not the best solution so we should try to make it work through the frontend with includes instead

--- src/main/java/fetchers/SearchFetcher.java
+++ src/main/java/fetchers/SearchFetcher.java
@@ -8,13 +8,18 @@ import DTO.HarryPotterListDTO;
 import DTO.StarWarsDTO;
 import DTO.StarWarsListDTO;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import utils.HttpUtils;
 
 public class SearchFetcher {
@@ -23,8 +28,7 @@ public class SearchFetcher {
     private static String hpApi = "http://hp-api.herokuapp.com/api/characters";
     private static String gotApi = "https://thronesapi.com/api/v2/Characters";
 
-    public static String searchFetcherApi(ExecutorService threadPool, final Gson gson) throws Exception {
-
+    public static CombinedListDTO getCombinedDTOList(ExecutorService threadPool, final Gson gson) throws InterruptedException, ExecutionException, TimeoutException {
         Callable<List<HarryPotterDTO>> hpTask = new Callable<List<HarryPotterDTO>>() {
             @Override
             public List<HarryPotterDTO> call() throws Exception {
@@ -77,9 +81,61 @@ public class SearchFetcher {
 
         CombinedListDTO combinedList = new CombinedListDTO(gotList, hpList, swList);
 
+        return combinedList;
+    }
+
+    public static String searchFetcherApi(ExecutorService threadPool, final Gson gson) throws Exception {
+
+        CombinedListDTO combinedList = getCombinedDTOList(threadPool, gson);
+
         String standartJSON = gson.toJson(combinedList);
 
         return standartJSON;
+    }
+
+    public static String searchForChar(ExecutorService threadPool, final Gson gson, String characterName) throws Exception {
+
+        CombinedListDTO combinedList = getCombinedDTOList(threadPool, gson);
+        List<StarWarsDTO> swCharList = new ArrayList();
+        List<GotDTO> gotCharList = new ArrayList();
+        List<HarryPotterDTO> hpCharList = new ArrayList();
+
+        for (int i = 0; i < combinedList.getSwList().getStarWarsDTOList().size(); i++) {
+            if (combinedList.getSwList().getStarWarsDTOList().get(i).getName().equalsIgnoreCase(characterName)) {
+                swCharList.add(combinedList.getSwList().getStarWarsDTOList().get(i));
+                System.out.println("This is the SW list: " + combinedList.getSwList().getStarWarsDTOList().get(i).getName());
+
+            }
+        }
+
+        for (int i = 0; i < combinedList.getGotList().getGotListDTO().size(); i++) {
+            if (combinedList.getGotList().getGotListDTO().get(i).getFullName().equalsIgnoreCase(characterName)) {
+                gotCharList.add(combinedList.getGotList().getGotListDTO().get(i));
+                System.out.println("This is the GOT list: " + combinedList.getGotList().getGotListDTO().get(i).getFullName());
+
+            }
+        }
+
+        for (int i = 0; i < combinedList.getHpList().getHpDTOList().size(); i++) {
+            if (combinedList.getHpList().getHpDTOList().get(i).getName().equalsIgnoreCase(characterName)) {
+                hpCharList.add(combinedList.getHpList().getHpDTOList().get(i));
+
+                System.out.println("This is the HP list: " + combinedList.getHpList().getHpDTOList().get(i).getName());
+
+            }
+        }
+
+        String jsonString = gson.toJson(swCharList) + gson.toJson(gotCharList) + gson.toJson(hpCharList);
+
+        return jsonString;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+        ExecutorService es = Executors.newCachedThreadPool();
+        String result = searchForChar(es, GSON, "Harry Potter");
+        System.out.println(result);
     }
 
 }
