9280b3d15e48763313829cbf11607c519f7a1243
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-12-11 10:01:55 +0100

    Added GotFetcherTest

--- src/main/java/cors/CorsFilter.java
+++ src/main/java/cors/CorsFilter.java
@@ -13,44 +13,45 @@ import javax.ws.rs.ext.Provider;
 @PreMatching
 public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
-  @Override
-  public void filter(ContainerRequestContext request) throws IOException {
-
-   //If it's a preflight request, abort the request with a 200 status, and the CORS headers are added in the
-   // response filter method below.
-   if (isPreflightRequest(request)) {
      request.abortWith(Response.ok().build());
-      return;
-    }
-  }
-    
-  // A preflight request is an OPTIONS request with an Origin header.
-  private static boolean isPreflightRequest(ContainerRequestContext request) {
-      return request.getHeaderString("Origin") != null  && request.getMethod().equalsIgnoreCase("OPTIONS");
-  }
+    @Override
+    public void filter(ContainerRequestContext request) throws IOException {
 
-  // Method for ContainerResponseFilter.
-  @Override
-  public void filter(ContainerRequestContext request, ContainerResponseContext response)
-          throws IOException {
-    // if there is no Origin header, then it is not a cross origin request - don't do anything.
-    if (request.getHeaderString("Origin") == null) {
-          return;
+        //If it's a preflight request, abort the request with a 200 status, and the CORS headers are added in the
+        // response filter method below.
+        if (isPreflightRequest(request)) {
+            request.abortWith(Response.ok().build());
+            return;
+        }
     }
 
-    // If it is a preflight request add all the CORS headers here.
-    if (isPreflightRequest(request)) {
-        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        response.getHeaders().add("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD");
-          response.getHeaders().add("Access-Control-Allow-Headers",
-          // Whatever other non-standard/safe headers (see list above) 
-          // you want the client to be able to send to the server,
-          // put it in this list. And remove the ones you don't want.
-          "Origin, Accept, Content-Type, Authorization,x-access-token");
+    // A preflight request is an OPTIONS request with an Origin header.
+    private static boolean isPreflightRequest(ContainerRequestContext request) {
+        return request.getHeaderString("Origin") != null && request.getMethod().equalsIgnoreCase("OPTIONS");
     }
 
-    /* Cross origin requests can be either simple requests or preflight request. We need to add this
+    // Method for ContainerResponseFilter.
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response)
+            throws IOException {
+        // if there is no Origin header, then it is not a cross origin request - don't do anything.
+        if (request.getHeaderString("Origin") == null) {
+            return;
+        }
+
+        // If it is a preflight request add all the CORS headers here.
+        if (isPreflightRequest(request)) {
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            response.getHeaders().add("Access-Control-Allow-Headers",
+                    // Whatever other non-standard/safe headers (see list above) 
+                    // you want the client to be able to send to the server,
+                    // put it in this list. And remove the ones you don't want.
+                    "Origin, Accept, Content-Type, Authorization,x-access-token");
+        }
+
+        /* Cross origin requests can be either simple requests or preflight request. We need to add this
      header to both types of requests. Only preflight requests need the previously added headers. */
-     response.getHeaders().add("Access-Control-Allow-Origin", "*");
-  }
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+    }
 }
---
+++ src/test/java/utils/GotFetcherTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.parsing.Parser;
+import java.net.URI;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import rest.ApplicationConfig;
+
+/**
+ *
+ * @author josef
+ */
+@Disabled
+public class GotFetcherTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/jpareststarter/api";
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        httpServer = startServer();
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        httpServer.shutdownNow();
+    }
+
+    @Test
+    public void testServerIsUp() {
+        System.out.println("Testing is server UP");
+        given().when().get("/got").then().statusCode(200);
+    }
+
+}
